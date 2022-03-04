6a891b411315a432145c332184d76ff59b023293
Luka Seamus Wright <47463990+lukaseamus@users.noreply.github.com>
2022-03-04 15:37:48 +0100

    Update Steromphala.R

--- Steromphala.R
+++ Steromphala.R
@@ -9,3 +9,8 @@ S <- read.csv("~/PATH/Steromphala.csv")
 require(psych)
 Lstats <- with(S, describeBy(Lower, Species, mat = T))
 Ustats <- with(S, describeBy(Upper, Species, mat = T))
+
+detach(package:psych)
+rm(list = ls())
+graphics.off()
+cat("\014")
