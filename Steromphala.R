35528f6ae01806e7829e4775b75bf19d1f3497bc
Luka Seamus Wright <47463990+lukaseamus@users.noreply.github.com>
2022-03-04 15:37:06 +0100

    Update Steromphala.R

--- Steromphala.R
+++ Steromphala.R
@@ -1,3 +1,9 @@
+###############################################################
+#### Ecology of Phorcus turbinatus                         ####
+#### Steromphala umbilicalis and S. cineraria distribution ####
+#### Luka Seamus Wright                                    ####
+###############################################################
+
 S <- read.csv("~/PATH/Steromphala.csv")
 
 require(psych)
